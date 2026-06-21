70529499f5ce4c1ca6083f42ba2fba8b35a73b57
Nedim <nedimteker84@gmail.com>
2026-06-21 20:20:23 +0300

    Add Farcaster account association

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -5,9 +5,11 @@ export async function GET() {
 
   return NextResponse.json({
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature: "",
+      header:
+        "eyJmaWQiOjc2OTM2OCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweEU5MWM5RWNiODkzOGE2RDc4Njg5MjM0NTEyN0YzOTJBQmMwRTdjNTIifQ",
+      payload: "eyJkb21haW4iOiJuZWRpbS1iYXNlLTMudmVyY2VsLmFwcCJ9",
+      signature:
+        "jjk/LnECYOLCXZ6I1hUxdoy9ja46aNyPDppkY9zrY3hyVlAlyqQj7v5v0WFhteeHyw7tzcvEUDRIqIyTTJSVohs=",
     },
 
     frame: {
