c1adc283a8adf8c28f808ee52be2d29927a550d1
Nedim <nedimteker84@gmail.com>
2026-06-22 12:48:27 +0300

    add badge metadata rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/badges/1.json",
+        destination: "/api/badges?id=1",
+      },
+      {
+        source: "/api/badges/2.json",
+        destination: "/api/badges?id=2",
+      },
+      {
+        source: "/api/badges/3.json",
+        destination: "/api/badges?id=3",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
